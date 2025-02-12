59a1b7d97a3d20ac547280e21a425875019c2434
Yanagi Hinano <takenoko2402@gmail.com>
2025-02-12 16:06:05 +0900

    vite.configはvitest.configに上書きされるため内容をvitest.configに転記

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vitest/config";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
   },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
