58da5d76258c2090b2a639971261f86d06028e1d
Yanagi Hinano <takenoko2402@gmail.com>
2025-02-13 18:19:13 +0900

    vite.configにaseを追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/front-end-cording-test-1",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
