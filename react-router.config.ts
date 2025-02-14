9ec2a693b8fbd2e586dc1e8bd2231c0690539e6d
Yanagi Hinano <takenoko2402@gmail.com>
2025-02-14 22:47:48 +0900

    Fix basename in react-router.config

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,4 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: false,
+  basename: process.env.ENV === "prod" ? "/front-end-cording-test-1/" : "/",
 } satisfies Config;
