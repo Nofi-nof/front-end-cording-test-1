787a6a4f9134ec0ec3ede3aa1c093865065cf7ae
Yanagi Hinano <takenoko2402@gmail.com>
2025-02-14 22:39:44 +0900

    baseを修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: process.env.ENV === "prod" ? "/front-end-cording-test-1/" : "/",
 });
