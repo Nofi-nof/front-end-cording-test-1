8943cd2daa2e285be800a0e3ebcc77f8076e717b
Yanagi Hinano <takenoko2402@gmail.com>
2025-02-16 23:32:21 +0900

    APIから県の情報を取得し、チェックボックスとして表示する

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router";
+import { useState } from "react";
 import type { Route } from "./+types/home";
 import { Main } from "./main/main";
 
@@ -9,6 +9,64 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Main />;
+type PrefecturesResponse = {
+  message: string;
+  result: Prefectures[];
+};
+
+interface Prefectures {
+  prefCode: number;
+  prefName: string;
+}
+
+export async function clientLoader() {
+  const url =
+    "https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/prefectures";
+  const apiKey = import.meta.env.VITE_API_KEY as string;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "x-api-key": apiKey,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  const { result } = (await response.json()) as PrefecturesResponse;
+
+  return result;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const [selectedPrefCode, setSelectedPrefCode] = useState<number[]>([]);
+  const handleCheckboxChange = (prefCode: number) => {
+    setSelectedPrefCode((prev) =>
+      prev.includes(prefCode)
+        ? prev.filter((item) => item !== prefCode)
+        : [...prev, prefCode],
+    );
+  };
+  return (
+    <>
+      <Main />
+      <ul>
+        {loaderData.map((pref) => (
+          <li key={pref.prefCode}>
+            <label>
+              <input
+                type="checkbox"
+                value={pref.prefCode}
+                onChange={() => handleCheckboxChange(pref.prefCode)}
+              />
+              {pref.prefCode}
+              {pref.prefName}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
