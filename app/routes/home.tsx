97c0499830df9daa68c5c232ea1dd81f0295c7ef
Yanagi Hinano <takenoko2402@gmail.com>
2025-02-20 23:46:24 +0900

    populationAPIから人口データを取得して選択した県ごとにグラフ化する

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,17 @@
-import { useState } from "react";
+import { PureComponent, useState, type Key } from "react";
 import type { Route } from "./+types/home";
 import { Main } from "./main/main";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,20 +22,41 @@ export function meta({}: Route.MetaArgs) {
 
 type PrefecturesResponse = {
   message: string;
-  result: Prefectures[];
+  result: Prefecture[];
 };
 
-interface Prefectures {
+interface Prefecture {
   prefCode: number;
   prefName: string;
 }
 
-export async function clientLoader() {
-  const url =
-    "https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/prefectures";
-  const apiKey = import.meta.env.VITE_API_KEY as string;
+interface PopulationData {
+  year: number;
+  value: number;
+}
 
-  const response = await fetch(url, {
+interface PopulationResponse {
+  message: string;
+  result: {
+    boundaryYear: number;
+    data: {
+      label: string;
+      data: PopulationData[];
+    }[];
+  };
+}
+
+type PopulationChartData = {
+  year: number;
+  [prefName: string]: number;
+};
+const apiKey = import.meta.env.VITE_API_KEY as string;
+
+export async function clientLoader() {
+  const prefecturesUrl =
+    "https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/prefectures";
+
+  const response = await fetch(prefecturesUrl, {
     method: "GET",
     headers: {
       "x-api-key": apiKey,
@@ -40,15 +72,18 @@ export async function clientLoader() {
   return result;
 }
 
+//チェックボックスの選択を配列にする
 export default function Home({ loaderData }: Route.ComponentProps) {
-  const [selectedPrefCode, setSelectedPrefCode] = useState<number[]>([]);
-  const handleCheckboxChange = (prefCode: number) => {
+  const [selectedPrefCode, setSelectedPrefCode] = useState<Prefecture[]>([]);
+  const handleCheckboxChange = (pref: Prefecture) => {
     setSelectedPrefCode((prev) =>
-      prev.includes(prefCode)
-        ? prev.filter((item) => item !== prefCode)
-        : [...prev, prefCode],
+      prev.includes(pref)
+        ? prev.filter((item) => item.prefCode !== pref.prefCode)
+        : [...prev, pref],
     );
   };
+  const queries = usePopulationQueries(selectedPrefCode);
+
   return (
     <>
       <Main />
@@ -59,7 +94,7 @@ export default function Home({ loaderData }: Route.ComponentProps) {
               <input
                 type="checkbox"
                 value={pref.prefCode}
-                onChange={() => handleCheckboxChange(pref.prefCode)}
+                onChange={() => handleCheckboxChange(pref)}
               />
               {pref.prefCode}
               {pref.prefName}
@@ -67,6 +102,86 @@ export default function Home({ loaderData }: Route.ComponentProps) {
           </li>
         ))}
       </ul>
+      {queries.map((query) => (
+        <ul key={query.data?.pref.prefCode}>
+          <li>
+            <div style={{ width: "100%", height: 300 }}>
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={query.data?.result}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="year" />
+                  <YAxis dataKey={query.data?.pref.prefName} />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey={query.data?.pref.prefName}
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          </li>
+        </ul>
+      ))}
     </>
   );
 }
+
+//県のコードを渡して、APIから人口データを受け取る
+const fetchPopulationApiBySelectedPrefCode = async (pref: Prefecture) => {
+  const response = await fetch(
+    `https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/population/composition/perYear?prefCode=${pref.prefCode}`,
+    {
+      method: "GET",
+      headers: {
+        "x-api-key": apiKey,
+      },
+    },
+  );
+  if (!response.ok) throw new Error("Failed to fetch PopulationAPI");
+
+  const data = (await response.json()) as PopulationResponse;
+
+  console.log(transformData(data, pref));
+
+  return transformData(data, pref);
+};
+
+const usePopulationQueries = (selectedPref: Prefecture[]) => {
+  //このdataとprefCodeを紐付けてオブジェクトにしたい
+  const results = useQueries({
+    queries: selectedPref.map((pref) => ({
+      queryKey: ["fetchPopulationApiBySelectedPrefCode", pref.prefCode],
+      queryFn: () => fetchPopulationApiBySelectedPrefCode(pref),
+    })),
+  });
+
+  return results;
+};
+
+const transformData = (apiData: PopulationResponse, pref: Prefecture) => {
+  const transformed = apiData.result.data[0].data.map(({ year, value }) => {
+    const transformed: PopulationChartData = { year };
+    // prefecturesのprefcode=ここのprefcodeのとき、prefectures.prefNameをtransformed[prefName] = population;
+    transformed[pref.prefName] = value;
+
+    return transformed;
+  });
+
+  return {
+    pref,
+    result: transformed,
+  };
+};
